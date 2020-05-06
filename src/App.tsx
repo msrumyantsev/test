fc6e32576256524dc662cbf3b603acfa76029e5a
maxx <msrumyantsev@gmail.com>
2020-05-06 13:48:58 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import Tab3 from './pages/Tab3';
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
 
+
 /* Basic CSS for apps built with Ionic */
 import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
